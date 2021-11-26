5aabaae36352c4ee90990a8f814ce0af6d4c5109
PAUL KETTLER <kettler51458@gotoltc.edu>
2021-11-26 10:28:36 -0600

    Initial Commit

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Cinema
+                if (!context.Cinemas.Any())
+                {
+
+                }
+                //Actors
+                if (!context.Actors.Any())
+                {
+
+                }
+                //Producers
+                if (!context.Producers.Any())
+                {
+
+                }
+                //Movies
+                if (!context.Movies.Any())
+                {
+
+                }
+                //Actors & Movies
+                if (!context.Actors_Movies.Any())
+                {
+
+                }
+
+            }
+        }
+    }
+}
