9a232702ec168d998c01ffa59f8ce87a6cb027fb
PAUL KETTLER <kettler51458@gotoltc.edu>
2021-11-29 17:51:04 -0600

    Tutorial progress through Adding new cinema to database

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eTickets.Data.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,12 +7,22 @@ using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
-    public class Actor
+    public class Actor:IEntityBase
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is Required")]
         public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is Required")]
         public string Bio { get; set; }
 
         //Relationships
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,6 +28,11 @@ namespace eTickets
         {
             //DbContext configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+
+            //Services configuration
+            services.AddScoped<IActorsService, ActorsService>();
+            services.AddScoped<IProducersService, ProducersService>();
+            services.AddScoped<ICinemasService, CinemasService>();
 
             services.AddControllersWithViews();
         }
@@ -57,6 +63,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
